f3356319f1d39ed44ce4c67df3ca7839451a5a92
TranMyPhuAn <128568357+TranMyPhuAn@users.noreply.github.com>
2023-04-21 14:44:15 +0700

    .

--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 // yarn add dotevn
 
 module.exports = {
-    host:       process.env.HOST,
-    userName:   process.env.USER,
-    pass:       process.env.PASS,
-    port:       process.env.PORT,
-    dialect:    process.env.DIALECT,
-    database:   process.env.DATABASE,
+    host:       process.env.DB_HOST,
+    userName:   process.env.DB_USERNAME,
+    pass:       process.env.DB_PASS,
+    port:       process.env.DB_PORT,
+    dialect:    process.env.DB_DIALECT,
+    database:   process.env.DB_DATABASE,
 }
